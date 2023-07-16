225a10015436e4dbe1faad62e6dc55e0998a7762
GoranVujnovic1985 <goran.vujnovic@nis.rs>
2023-07-16 17:26:25 +0200

    added Union

--- Main.c
+++ Main.c
@@ -5,10 +5,18 @@ const char korisnik1[] = "Aleksandar Aleksic";
 
 const char korisnik2[] = "Bojan Bojanic";
 
+
+typedef union
+{
+	char mestoStanovanja[20];
+
+}prebivaliste;
+
 typedef struct
 {
 	char brojTelefona[15];
 	char *korisnik;
+	prebivaliste prebivaliste;
 	struct lista *next;
 
 }lista;
@@ -21,10 +29,12 @@ void popuniListu(void)
 {
 	strcpy(prvi.brojTelefona, "064/888-11-11");
 	prvi.korisnik = korisnik1;
+	strcpy(prvi.prebivaliste.mestoStanovanja, "Novi Sad");
 	prvi.next = &drugi;
 
 	strcpy(drugi.brojTelefona, "064/888-11-12");
 	drugi.korisnik = korisnik2;
+	strcpy(drugi.prebivaliste.mestoStanovanja, "Petrovaradin");
 	drugi.next = NULL;
 
 
@@ -33,7 +43,7 @@ void popuniListu(void)
 	while(ptr_list != NULL)
 	{
 
-	printf("Korisnik %s ima sledeci broj telefona: %s\n", ptr_list->korisnik , ptr_list->brojTelefona);
+	printf("Korisnik %s stanuje u %s i ima sledeci broj telefona: %s\n", ptr_list->korisnik ,ptr_list->prebivaliste.mestoStanovanja, ptr_list->brojTelefona);
 	ptr_list = ptr_list->next;
 
 	}
