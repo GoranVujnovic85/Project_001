3038ebe1b5899cb8641ee0a3d48440dc9e5f1359
GoranVujnovic1985 <goran.vujnovic@nis.rs>
2023-07-16 12:51:06 +0200

    Telefonski_Imenik

---
+++ Main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+const char korisnik1[] = "Aleksandar Aleksic";
+
+
+typedef struct
+{
+	char brojTelefona[15];
+	char *korisnik;
+	struct lista *next;
+
+}lista;
+
+
+lista prvi;
+
+void popuniListu(void)
+{
+	strcpy(prvi.brojTelefona, "064/888-11-11");
+	prvi.korisnik = korisnik1;
+	prvi.next = NULL;
+
+	printf("Korisnik %s ima sledeci broj telefona: %s\n", prvi.korisnik , prvi.brojTelefona);
+
+}
+
+void main()
+{
+
+	popuniListu();
+}
+
