b82f0d9f424b0931cc5c67515b8cfab587fc546b
GoranVujnovic1985 <goran.vujnovic@nis.rs>
2023-07-16 14:22:10 +0200

    made functionality for another user

--- Main.c
+++ Main.c
@@ -2,7 +2,7 @@
 #include<string.h>
 
 const char korisnik1[] = "Aleksandar Aleksic";
-
+const char korisnik2[] = "Bojan Bojanic";
 
 typedef struct
 {
@@ -14,15 +14,20 @@ typedef struct
 
 
 lista prvi;
+lista drugi;
 
 void popuniListu(void)
 {
 	strcpy(prvi.brojTelefona, "064/888-11-11");
 	prvi.korisnik = korisnik1;
-	prvi.next = NULL;
+	prvi.next = &drugi;
+
+	strcpy(drugi.brojTelefona, "064/888-11-12");
+	drugi.korisnik = korisnik2;
+	drugi.next = NULL;
 
 	printf("Korisnik %s ima sledeci broj telefona: %s\n", prvi.korisnik , prvi.brojTelefona);
-
+	printf("Korisnik %s ima sledeci broj telefona: %s\n", drugi.korisnik , drugi.brojTelefona);
 }
 
 void main()
